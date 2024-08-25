335ffd29ffccc6726fda900edda69da4ad3b3f11
Saumo05 <saumodeepdutta@gmail.com>
2024-08-25 12:29:01 +0530

    sixth commit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   const handleSubmit = async () => {
     try {
       const res = await axios.post(
-        "http://localhost:3000/bfhl",
+        "https://bhfl-backend-drab.vercel.app/",
         JSON.parse(jsonInput)
       );
 
